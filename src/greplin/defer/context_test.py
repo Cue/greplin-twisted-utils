e8609c7e81422774dccf2a4a628065f1971554f4
Kevin Clark <kevinc@greplin.com>
2012-08-17 16:17:33 -0700

    thread locals / fix intermingled deferred contexts
    Fix for intermingling deferred contexts happens via our inline callbacks
    implementation, so code using context needs to use greplin inline
    callbacks across the board.

--- src/greplin/defer/context_test.py
+++ src/greplin/defer/context_test.py
@@ -13,8 +13,10 @@
 # limitations under the License.
 
 """Tests for context storage and restoration."""
+from twisted.internet.defer import DeferredList
 
 from greplin.defer import context, inline
+from greplin.defer.time import sleep
 from greplin.testing.base import BaseDeferredTest
 
 context.install()
@@ -38,8 +40,8 @@ class ContextTest(BaseDeferredTest):
 
 
 
-class ADeferToThreadContextTest(BaseDeferredTest):
-  """Tests for context with deferToThread"""
+class ConcurrentContextTests(BaseDeferredTest):
+  """Tests for context with deferreds"""
 
 
   @inline.callbacks
@@ -69,3 +71,51 @@ class ADeferToThreadContextTest(BaseDeferredTest):
     """This is actually confirmation that testContextIsRestoredWhenExceptionsThrown didn't much about in the parent
     context. The Z in the test name is to ensure it goes afterwards."""
     yield self.assertRaises(KeyError, context.get, 'foo')
+
+
+  @inline.callbacks
+  def notCrashySleep(self, sleepFor, **kwargs):
+    """Sleeps for sleepFor seconds and asserts that kwargs is equal to the stored context variables"""
+    with context.set(**kwargs):
+      yield sleep(sleepFor)
+      self.assertEquals(kwargs, context.all())
+
+
+  @inline.callbacks
+  def testDoesntMingleContextAcrossUnyieldedDeferred(self):
+    """Deferreds that exist at the same time should not have interacting contexts"""
+    self.assertFalse(context.has('a'))
+    self.assertFalse(context.has('b'))
+    a = self.notCrashySleep(0.01, a=1)
+    self.assertFalse(context.has('a'))
+    self.assertFalse(context.has('b'))
+    b = self.notCrashySleep(0.001, b=2)
+    self.assertFalse(context.has('a'))
+    self.assertFalse(context.has('b'))
+
+    yield sleep(0.02)
+    yield DeferredList([a, b])
+
+
+  @inline.callbacks
+  def notCrashySleepWithNonRootContext(self, sleepFor, parentCtx, **kwargs):
+    """Sleeps for sleepFor seconds and then asserts that kwargs + parentCtx is the current context"""
+    expected = {}
+    expected.update(parentCtx)
+    expected.update(kwargs)
+    with context.set(**kwargs):
+      yield sleep(sleepFor)
+      self.assertEquals(expected, context.all())
+
+
+  @inline.callbacks
+  def testMaintainsNonRootContextAcrossUnyieldedDeferred(self):
+    """Deferreds that exist at the same time under a parent context should not interfere and have that parent context"""
+
+    with context.set(parent=1):
+      a = self.notCrashySleepWithNonRootContext(0.01, {'parent': 1}, a=1)
+      b = self.notCrashySleepWithNonRootContext(0.001, {'parent': 1}, b=2)
+
+    yield sleep(0.02)
+    yield DeferredList([a, b])
+
